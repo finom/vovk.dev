106ef8fa3acf9c6d9a9b204fc7281fd8bf4f90a2
Andrii Gubanov <andrii@gubanov.eu>
2024-02-29 20:11:07 +0100

    fix: Minor mobile fix

--- src/components/Examples/Example/WorkerExample.tsx
+++ src/components/Examples/Example/WorkerExample.tsx
@@ -3,16 +3,17 @@ import { useEffect, useState } from 'react';
 import { WorkerService } from 'vovk-examples';
 
 export default function WorkerExample() {
-  const isMobile = typeof document !== 'undefined' && 'ontouchstart' in document.documentElement;
-  const [value, setValue] = useState(
-    // use smaller number on mobile devices
-    isMobile ? '333944026345847228099687' : '337751842839865299034216387'
-  );
+  const [value, setValue] = useState('337751842839865299034216387');
   const [result, setResult] = useState<bigint[]>();
   const [isCalculating, setIsCalculating] = useState(false);
   const regExp = /^-?\d+$/;
 
   useEffect(() => {
+    const isMobile = typeof document !== 'undefined' && 'ontouchstart' in document.documentElement;
+    if (isMobile) {
+      setValue('333944026345847228099687');
+    }
+
     WorkerService.use(new Worker(new URL('vovk-examples/dist/WorkerService.js', import.meta.url)));
   }, []);
 
