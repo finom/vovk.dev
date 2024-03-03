0d141b3dbaf98a613e05beab9cd1ab6f6f915087
Andrii Gubanov <andrii@gubanov.eu>
2024-03-03 20:58:01 +0100

    fix: Replace a feature block

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -62,7 +62,7 @@ const SolvedProblems = () => {
             </p>
           </div>
         </div>
-        <div className="flex gap-4 items-start flex-col ">
+        {/*<div className="flex gap-4 items-start flex-col ">
           <span className="text-rose-600 bg-rose-500/10 rounded-full w-[44px] h-[44px] items-center justify-center flex">
             <svg
               width="24px"
@@ -132,6 +132,67 @@ const SolvedProblems = () => {
                 Customization & Configuration
               </Link>
               . The rest are optional.
+            </p>
+          </div>
+        </div>*/}
+        <div className="flex gap-4 items-start flex-col ">
+          <span className="text-rose-600 bg-rose-500/10 rounded-full w-[44px] h-[44px] items-center justify-center flex">
+            <svg
+              width="24px"
+              height="24px"
+              strokeWidth="1.5"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M21 2L20 3"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path d="M3 2L4 3" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+              <path
+                d="M21 16L20 15"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M3 16L4 15"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path d="M9 18H15" stroke="currentColor" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+              <path
+                d="M10 21H14"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+              <path
+                d="M11.9998 3C7.9997 3 5.95186 4.95029 5.99985 8C6.02324 9.48689 6.4997 10.5 7.49985 11.5C8.5 12.5 9 13 8.99985 15H14.9998C15 13.0001 15.5 12.5 16.4997 11.5001L16.4998 11.5C17.4997 10.5 17.9765 9.48689 17.9998 8C18.0478 4.95029 16 3 11.9998 3Z"
+                stroke="currentColor"
+                strokeWidth="1.5"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              />
+            </svg>
+          </span>
+          <div>
+            <h3 className="font-semibold text-xl">Minimal Abstrctions</h3>
+            <p className="mt-1 text-secondary">
+              Vovk.ts is specially designed for Next.js App Router. It doesn't introduce complex abstractions being just
+              a wrapper over Next.js{' '}
+              <Link target="_blank" href="https://nextjs.org/docs/api-routes/introduction" className="link">
+                route handlers
+              </Link>
+              .
             </p>
           </div>
         </div>
