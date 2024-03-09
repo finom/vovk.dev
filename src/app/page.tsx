1480cdc3387a0a7242a3ddccc798842b5683278e
Andrii Gubanov <andrii@gubanov.eu>
2024-03-09 10:30:35 +0100

    fix: Add support Ukraine now, remove Discoed link for now

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,71 +17,78 @@ const inter = Inter({
 
 const Home = () => {
   return (
-    <div className={`${inter.className} px-4`}>
-      <header className="flex items-center justify-between px-5 py-4">
-        <div className="flex items-center gap-2">
-          <a className="flex" href="/">
-            <VovkTextLogo width={150} />
-          </a>
-        </div>
-        <TopNav />
-      </header>
-      {/* <OgToScreenshot /> */}
-      <Jumbotron />
-      <CreateInitUse />
-      <ManualInstall />
-      <Features />
-      <BonusFeatures />
-      <Examples />
+    <>
+      <div className="text-center border-b py-1 border-gray-150 dark:border-gray-800">
+        <Link href="https://supportukrainenow.org/" target="_blank" className="font-semibold">
+          🌻 Support Ukraine Now! 🇺🇦
+        </Link>
+      </div>
+      <div className={`${inter.className} px-4`}>
+        <header className="flex items-center justify-between px-5 py-4">
+          <div className="flex items-center gap-2">
+            <a className="flex" href="/">
+              <VovkTextLogo width={150} />
+            </a>
+          </div>
+          <TopNav />
+        </header>
+        {/* <OgToScreenshot /> */}
+        <Jumbotron />
+        <CreateInitUse />
+        <ManualInstall />
+        <Features />
+        <BonusFeatures />
+        <Examples />
 
-      <div className="max-w-3xl mx-auto px-5 mt-28">
-        <div className="flex flex-col justify-center">
-          <div className="text-center">
-            <h2 className="font-semibold text-3xl">Sponsors</h2>
-            <p className="max-w-md mx-auto mt-2 text-secondary">
-              <Link href="https://github.com/sponsors/finom" target="_blank" className="link mb-2 inline-block">
-                Sponsor the author of this project on&nbsp;Github&nbsp;♥️
-              </Link>
-              <br />
-              You can also contact me via email from my{' '}
-              <Link href="https://github.com/finom" className="link" target="_blank" rel="noopener">
-                Github profile
-              </Link>
-            </p>
+        <div className="max-w-3xl mx-auto px-5 mt-28">
+          <div className="flex flex-col justify-center">
+            <div className="text-center">
+              <h2 className="font-semibold text-3xl">Sponsors</h2>
+              <p className="max-w-md mx-auto mt-2 text-secondary">
+                <Link href="https://github.com/sponsors/finom" target="_blank" className="link mb-2 inline-block">
+                  Sponsor the author of this project on&nbsp;Github&nbsp;♥️
+                </Link>
+                <br />
+                You can also contact me via email from my{' '}
+                <Link href="https://github.com/finom" className="link" target="_blank" rel="noopener">
+                  Github profile
+                </Link>
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-      <div className="border-t border-gray-200 dark:border-gray-700 mt-20">
-        <footer>
-          <div className="flex flex-col md:flex-row gap-3 items-center justify-between py-4 md:py-8 max-w-screen-2xl mx-auto text-sm px-5 text-secondary">
-            <p>
-              Created by{' '}
-              <a href="https://github.com/finom" className="link" target="_blank" rel="noopener">
-                Andrii Gubanov
-              </a>
-            </p>
+        <div className="border-t border-gray-200 dark:border-gray-700 mt-20">
+          <footer>
+            <div className="flex flex-col md:flex-row gap-3 items-center justify-between py-4 md:py-8 max-w-screen-2xl mx-auto text-sm px-5 text-secondary">
+              <p>
+                Created by{' '}
+                <a href="https://github.com/finom" className="link" target="_blank" rel="noopener">
+                  Andrii Gubanov
+                </a>
+              </p>
 
-            <nav className="text-center flex flex-col gap-3 sm:flex-row sm:gap-5">
-              <a href="https://docs.vovk.dev" className="link" target="_blank" rel="noopener noreferrer">
-                Vovk.ts Docs
-              </a>
-              <a href="https://github.com/finom/vovk" className="link" target="_blank" rel="noopener noreferrer">
-                Vovk.ts on Github
-              </a>
-              <a href="https://github.com/finom/vovk.dev" className="link" target="_blank" rel="noopener noreferrer">
-                This website on Github
-              </a>
-              <a href="https://iconoir.com/" className="link" target="_blank" rel="noopener noreferrer">
-                Icons
-              </a>
-              <a href="https://bright.codehike.org/" className="link" target="_blank" rel="noopener noreferrer">
-                Syntax Highlighting
-              </a>
-            </nav>
-          </div>
-        </footer>
+              <nav className="text-center flex flex-col gap-3 sm:flex-row sm:gap-5">
+                <a href="https://docs.vovk.dev" className="link" target="_blank" rel="noopener noreferrer">
+                  Vovk.ts Docs
+                </a>
+                <a href="https://github.com/finom/vovk" className="link" target="_blank" rel="noopener noreferrer">
+                  Vovk.ts on Github
+                </a>
+                <a href="https://github.com/finom/vovk.dev" className="link" target="_blank" rel="noopener noreferrer">
+                  This website on Github
+                </a>
+                <a href="https://iconoir.com/" className="link" target="_blank" rel="noopener noreferrer">
+                  Icons
+                </a>
+                <a href="https://bright.codehike.org/" className="link" target="_blank" rel="noopener noreferrer">
+                  Syntax Highlighting
+                </a>
+              </nav>
+            </div>
+          </footer>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
