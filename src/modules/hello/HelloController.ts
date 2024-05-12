228ee4f416cae04cf3745a875a12f17933618a5c
Andrii Gubanov <andrii@gubanov.eu>
2024-05-12 12:33:54 +0200

    docs: Vovk 2.1

--- src/modules/hello/HelloController.ts
+++ src/modules/hello/HelloController.ts
@@ -1,9 +1,11 @@
+import { NextResponse } from 'next/server';
 import { get, prefix } from 'vovk';
 
 @prefix('hello')
 export default class HelloController {
   @get('greeting.json')
   static getHello() {
+    // NextResponse.json({ greeting: 'Hello world!' }, { headers: { 'x-test': 'world' } });
     return { greeting: 'Hello world!' };
   }
 }
